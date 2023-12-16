dbba9d8326ad969278ef63d36abc0efc50da0f28
NikoGB <114112855+NikoGB@users.noreply.github.com>
2023-12-16 17:51:33 -0300

    Update Interpreter.js

--- frontend/src/utils/Interpreter.js
+++ frontend/src/utils/Interpreter.js
@@ -9,14 +9,22 @@ const objImgs = [
     { type: 'Conductor', img: '/objects/conductor.png' },
 ]
 
+const specialBehavior = [
+    { type: 'Pasajero', with: 'Bus', toAdd: [{ type: 0, value: { ratio: 0.2 } }] },
+    { type: 'Conductor', with: 'Bus', toAdd: [{ type: 0, value: { ratio: 0.2 } }] },
+    { type: 'Bus', with: 'Pasajero', toAdd: [{ type: 0, value: { ratio: 0.9 } }] }
+]
+
+
 const Interpreter = async (objectsList, maxFrames) => {
     const frames = []
 
     for (let i = 0; i < maxFrames; i++) {
         const asyncList = objectsList.filter((el) => el.startFrame <= i).map(async (obj) => {
-            if(obj.startFrame == i){
+            if (obj.startFrame == i) {
                 let state = obj.changes.length == 0 ? obj.value : obj.changes[0];
-                if (i == 0 && !primitivos.includes(obj.type)) {
+
+                if (!primitivos.includes(obj.type)) {
                     const index = objImgs.findIndex((objImg) => objImg.type == obj.type);
                     if (index > -1) {
                         var img = new Image();
@@ -52,14 +60,17 @@ const Interpreter = async (objectsList, maxFrames) => {
 
                 }
 
+                state = {...state, ...getSpecialValues(obj, objectsList)}
+
+
                 return { id: obj.id, state, type: obj.type }
-            } else if (obj.startFrame + obj.changes.length > i + 1) { 
+            } else if (obj.startFrame + obj.changes.length > i + 1) {
 
-                return { id: obj.id, state : obj.changes[i - obj.startFrame], type: obj.type }
+                return { id: obj.id, state: obj.changes[i - obj.startFrame], type: obj.type }
 
-            } else if (obj.startFrame + obj.changes.length == i + 1) { 
+            } else if (obj.startFrame + obj.changes.length == i + 1) {
 
-                return { id: obj.id, state : {...obj.value, parent : obj.lastValue.parent}, type: obj.type }
+                return { id: obj.id, state: { ...obj.value, parent: obj.lastValue.parent }, type: obj.type }
             } else {
                 return { id: obj.id, deleted: true }
             }
@@ -69,24 +80,56 @@ const Interpreter = async (objectsList, maxFrames) => {
 
         frames.push(auxObjectStates)
     }
-    frames.push(objectsList.map((obj)=>{ //BECAUSE IT MAY BE THAT THERE IS NOT A WAITIGN AT THE END
-        if(obj.value != null){
+
+    frames.push(objectsList.map((obj) => { //BECAUSE IT MAY BE THAT THERE IS NOT A WAITIGN AT THE END
+
+        if (obj.value != null) {
             const index = objImgs.findIndex((objImg) => objImg.type == obj.type);
             if (index > -1) {
                 const img = new Image();
                 img.src = objImgs[index].img;
-                return { id: obj.id, state : {...obj.value, parent : obj.lastValue.parent, img}, type: obj.type } 
+                return { id: obj.id, state: { ...obj.value, parent: obj.lastValue.parent, img, ...getSpecialValues(obj, objectsList) }, type: obj.type }
 
-            }else{
-                return { id: obj.id, state : {...obj.value, parent : obj.lastValue.parent}, type: obj.type } 
+            } else {
+                return { id: obj.id, state: { ...obj.value, parent: obj.lastValue.parent, ...getSpecialValues(obj, objectsList) }, type: obj.type }
 
             }
 
-        }else{
-            return { id: obj.id, deleted: true } 
+        } else {
+            return { id: obj.id, deleted: true }
         }
     }))
+
     return frames
+}
+
+const getSpecialValues = (obj, objectsList) => {
+    let vals = {}
+    const speIndex = specialBehavior.findIndex((special) => special.type == obj.type);
+    if (speIndex > -1) {
+        if (specialBehavior[speIndex].with) {
+            if (objectsList.some((lObj) => lObj.type == specialBehavior[speIndex].with)) {
+
+                specialBehavior[speIndex].toAdd.forEach((toAdd) => {
+                    if (toAdd.type == 0) {
+                        vals = { ...vals, ...toAdd.value }
+                    } else {
+                    }
+                })
+
+            }
+        } else {
+            specialBehavior[speIndex].toAdd.forEach((toAdd) => {
+                if (toAdd.type == 0) {
+                    vals = { ...vals, ...toAdd.value }
+                } else {
+                }
+            })
+        }
+        return vals
+    }else{
+        return {}
+    }
 }
 
 export default Interpreter
