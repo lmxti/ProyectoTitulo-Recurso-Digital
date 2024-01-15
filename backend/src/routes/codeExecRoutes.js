05e6e7173504d8631f5e48035f7d22cba3f6ffbf
NikoGB <114112855+NikoGB@users.noreply.github.com>
2024-01-14 21:29:09 -0300

    Delete backend directory

--- backend/src/routes/codeExecRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const codeExecController = require('../controllers/codeExecController');
-
-router.post('/', codeExecController.executeCode)
-
-module.exports = router;
